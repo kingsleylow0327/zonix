3a0cc6a912198ae00f877ceac4a2086967c92100
kingsleylow0327 <kingsleylow94@gmail.com>
2023-04-15 23:30:19 +0800

    trail and error 2

--- handler/trading_stop.py
+++ handler/trading_stop.py
@@ -5,7 +5,6 @@ def h_trading_stop(dbcon, player_id, order_dto):
     # Get related follower
     api_pair_list = dbcon.get_followers_api(player_id)
     multipier = 1.01 if order_dto.side == "Buy" else 0.99
-    order_dto.target_price = order_dto.target_price * multipier
     if api_pair_list == None or len(api_pair_list) == 0:
         return "StopLoss Shifted (NR)"
     
@@ -18,11 +17,12 @@ def h_trading_stop(dbcon, player_id, order_dto):
             if item["side"] == order_dto.side:
                 stop_px = item["entry_price"]
                 price_decimal = str(stop_px)[::-1].find('.')
+                order_dto.target_price = stop_px
+                order_dto.stop_loss = order_dto.target_price * multipier
                 order_dto.target_price = round(order_dto.target_price, price_decimal)
                 order_dto.stop_loss = round(order_dto.stop_loss, price_decimal)
                 order_dto.quantity = item["size"]
-                print(order_dto)
-                # order_dto.side = "Buy" if order_dto.side == "Sell" else "Buy"
+                order_dto.side = "Buy" if order_dto.side == "Sell" else "Buy"
                 place_order(session,
                             order_dto,
                             market_out=False,
