c3628914788a358c592d0644404c081326b1c622
kingsleylow0327 <kingsleylow94@gmail.com>
2023-09-14 00:04:55 +0800

    error handling for check price

--- handler/check_price.py
+++ handler/check_price.py
@@ -4,4 +4,7 @@ import json
 def h_check_price(symbol):
     api = f"https://api.bybit.com/spot/quote/v1/ticker/price?symbol={symbol.upper()}"
     r = requests.get(api)
-    return json.loads(r.content)["result"]["price"]
+    try:
+        return json.loads(r.content).get("result").get(["price"])
+    except:
+        return "(error geting price from bybit)" 
